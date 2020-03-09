93c05185ca312e01a147a114d6f3b8bb3f989050
Mikko Saari <mikko@mikkosaari.fi>
2020-03-09 09:59:51 +0200

    Adds an uninstall method

---
+++ uninstall.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * /uninstall.php
+ *
+ * @package Relevanssi Light
+ * @author  Mikko Saari
+ * @license https://wordpress.org/about/gpl/ GNU General Public License
+ * @see     https://www.relevanssi.com/light/
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
+
+global $wpdb;
+
+$sql = "ALTER TABLE $wpdb->posts DROP COLUMN `relevanssi_light_data` LONGTEXT AFTER `comment_count`";
+$wpdb->query( $sql ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared, WordPress.DB.DirectDatabaseQuery.NoCaching, WordPress.DB.DirectDatabaseQuery
+
+$sql = "ALTER TABLE $wpdb->posts DROP INDEX `relevanssi_light_fulltext`";
+$wpdb->query( $sql ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared, WordPress.DB.DirectDatabaseQuery.NoCaching, WordPress.DB.DirectDatabaseQuery
+
+delete_option( 'relevanssi_light' );
